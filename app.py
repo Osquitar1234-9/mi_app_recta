ee936e91ef1834a39249929d1c106b2f0cf57df9
Osquitar1234-9 <javier.decarpio.789@gmail.com>
2025-05-21 16:58:47 -0500

    Update app.py
    Fix port for Render

--- app.py
+++ app.py
@@ -68,4 +68,8 @@ def update_graph(m, b):
     return fig, intercept_text, warning
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 8050))
+    app.run(host='0.0.0.0', port=port, debug=True)
